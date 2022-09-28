688a637376e2d0c03bdcf0748b705361e3c05c32
Gilson Souza <br.gilson.sp@gmail.com>
2022-09-27 21:39:16 -0300

    update server-rest

--- server-rest/src/main/java/br/com/gilson/examples/serverrest/controller/ServerFreeController.java
+++ server-rest/src/main/java/br/com/gilson/examples/serverrest/controller/ServerFreeController.java
@@ -3,11 +3,15 @@ package br.com.gilson.examples.serverrest.controller;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +21,8 @@ import com.fasterxml.jackson.core.PrettyPrinter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
+import br.com.gilson.examples.serverrest.model.ObjectReturn;
+
 @RestController
 public class ServerFreeController {
 	
@@ -24,6 +30,25 @@ public class ServerFreeController {
 
 	@Autowired
 	private ObjectMapper mapper;
+	
+	@GetMapping("/wspr/pedidosub")
+	public List<ObjectReturn> getObjects(){
+		ObjectReturn object = new ObjectReturn();
+		ObjectReturn objectII = new ObjectReturn();
+		List<ObjectReturn> list = new ArrayList<>();
+		list.add(object);
+		list.add(objectII);
+		return list;
+	}
+
+	@GetMapping("/test")
+	public Map<String, Object> getAnyBody(){
+		Map<String, Object> response = new HashMap<>();
+		response.put("field_one", "value_one");
+		response.put("field_two", "value_two)");
+
+		return response;
+	}
 	
 	@PostMapping("/test")
 	public String invokeMap(@RequestBody Map<String, Object> in) {
